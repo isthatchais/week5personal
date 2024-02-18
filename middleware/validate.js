50734ddc811098fcd7121e40c4919f4ee00a46ae
Chais Chang <cha96001@byui.edu>
2024-02-17 21:09:26 -0700

    corrected reg ex

--- middleware/validate.js
+++ middleware/validate.js
@@ -2,16 +2,16 @@ const validator = require('../helpers/validate');
 
 const savePatient = (req, res, next) => {
   const validationRule = {
-    firstName: 'required|string',
-    lastName: 'required|string',
-    diagnosis: 'required|string',
-    birthday: 'required|string',
-    weightBearingStatus: 'required|string',
-    therapyOrderEndDate: 'required|string',
-    lastVisit: 'required|string',
-    nextVisit: 'required|string',
-    therapyGoals: 'required|string',
-    assignedNurse: 'required|string',
+    firstName: ['required', 'regex:/^[A-Za-z]{2,}$'],
+    lastName: ['required', 'regex:/^[A-Za-z]{3,}$'],
+    diagnosis: ['required', 'regex:/^[A-Za-z ]{3,}$'],
+    birthday: ['required', 'regex:/^[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$'],
+    weightBearingStatus: ['required', 'regex:/^[A-Za-z ]{3,}$'],
+    therapyOrderEndDate: ['required', 'regex:/^[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$'],
+    lastVisit: ['required', 'regex:/^[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$'],
+    nextVisit: ['required', 'regex:/^[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$'],
+    therapyGoals: ['required', 'regex:/^[A-Za-z ]{3,}$'],
+    assignedNurse: ['required', 'regex:/^[A-Za-z]{2,}$'],
   };
   validator(req.body, validationRule, {}, (err, status) => {
     if (!status) {
